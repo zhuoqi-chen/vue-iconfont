a3648af6c4bded3126096f5087e1830c215d3f5b
chenzhuoqi <chenzhuoqi@patsnap.com>
2017-09-03 13:39:07 +0800

    Uniform writing mode

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 //icon-font
 import SvgIcon from './components/svg-icon.vue';
 import FontIcon from './components/font-icon.vue';
-require('./css/icon.css')
+import './css/icon.css';
 import './css/icon-font/iconfont.css';
 import './css/icon-font/iconfont.js';
 
